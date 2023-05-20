dd32a0cc106abd7b5ce3913e2be4ad5c535a19e6
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-05-20 20:29:40 +0200

    boolean and conditionals

--- boolean-conditional/script.js
+++ boolean-conditional/script.js
@@ -35,3 +35,28 @@ if(10 !== 11) {
 } else {
   console.log('no');
 };
+
+// logical operator
+// in a comparation if the first value is false it returns
+// the falsey value and don't verify the rest. If it's true it returns the last true value.
+var x = (5 - 4) && (10 - 10);
+var y = (5 - 4) || (2 - 3) && (5 - 5)
+if(x) {
+  console.log('true exemple');
+} else if(5 <= 5) {
+  console.log('on going');
+} else {
+  console.log('no');
+};
+
+var color = 'yellow'
+switch (color) {
+  case 'blue':
+    console.log('Look at the sky');
+    break;
+  case 'yellow':
+    console.log('look at the sun');
+    break;
+  default:
+    console.log('pick a color');
+};
