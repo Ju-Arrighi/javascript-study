396363fb2294008c2bad4d9b02ecd4273f281e7b
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-05-20 23:41:56 +0200

    array, for, while

---
+++ arrays-loops/script.js
@@ -0,0 +1,15 @@
+var videoGames = ['Xbox', 'Nintendo DS', 'PlayStation'];
+
+var last = videoGames.pop();
+
+videoGames.push('Wii')
+
+for (var number = 0; number < 10; number++) {
+  console.log(videoGames);
+}
+console.log('break')
+var n = 0
+while(n < 10) {
+  console.log(videoGames);
+  n++
+}
