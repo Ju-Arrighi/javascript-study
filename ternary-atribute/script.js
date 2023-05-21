9ff8b21af7ffe1e2d0e8338a0e90406d7753151c
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-05-21 11:27:14 +0200

    ternary

---
+++ ternary-atribute/script.js
@@ -0,0 +1,14 @@
+var x = 5;
+var y = 10;
+x += y; // x = x + y
+x -= y; // x = x - y
+x *= y; // x = x * y
+x /= y; // x = x / y
+console.log(x += y)
+
+// Ternary
+
+age = 19;
+var underAge = age >= 18 ? 'adult' : 'under age';
+// If returns true or false its not necessary to right on the code
+var underAge = age >= 18
